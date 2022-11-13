3fe7d51d4b1f82fd54582fadc6eecf56ae2a6df2
Nguyen Gia Hoang <popemkt1@gmail.com>
2022-11-13 09:46:56 +0700

    Add Api versioning project

---
+++ dotnet/configurations/ApiVersioningSwagger/Controllers/WeatherForecastController.V2.cs
@@ -0,0 +1,29 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers;
+
+[ApiController]
+[ApiVersion("2")]
+[Route("w")]
+public class WeatherForecastControllerV2 : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastControllerV2> _logger;
+
+    public WeatherForecastControllerV2(ILogger<WeatherForecastControllerV2> logger)
+    {
+        _logger = logger;
+    }
+
+    [MapToApiVersion("2")]
+    [HttpGet(Name = "GetWeatherForecast")]
+    public string GetV2()
+    {
+        return "v2";
+    }
+}
