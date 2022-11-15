bb2384dd9123d575231f3726ec679650972d0411
Nguyen Gia Hoang <popemkt1@gmail.com>
2022-11-15 22:25:07 +0700

    Added Clean Arch Ardalis

---
+++ src/dotnet/templates/Ardalis/CleanArchitecture/src/CleanArchitecture.Infrastructure/FakeEmailSender.cs
@@ -0,0 +1,11 @@
+﻿using CleanArchitecture.Core.Interfaces;
+
+namespace CleanArchitecture.Infrastructure;
+
+public class FakeEmailSender : IEmailSender
+{
+    public Task SendEmailAsync(string to, string from, string subject, string body)
+    {
+        return Task.CompletedTask;
+    }
+}
